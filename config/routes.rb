079fe90a16b41f6da0c34c578d936b789964a680
saraabdalla238 <saraabdallahamd@gmail.com>
2022-05-18 22:18:36 +0200

    first LAB

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'articles/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "articles#index"
+  resources :articles
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+end
